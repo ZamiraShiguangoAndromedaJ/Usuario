61430884f23db048c1b41a5f7bd1a02fd91b032e
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-05 10:06:14 -0500

    usuario

---
+++ src/main/java/zamira/estudiante/usuario/Usuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zamira.estudiante.usuario;
+
+/**
+ *
+ * @author user
+ */
+public class Usuario {
+
+    public static void main(String[] args) {
+        System.out.println("hola Zamira");
+    }
+}
